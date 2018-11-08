523d0af7920283dc84d318cf4adc708ed95bc6f0
whandke <handkewojciech@gmail.com>
2018-11-08 17:06:26 +0100

    Updated .gitignore and added export to .PNG

--- planecognition.py
+++ planecognition.py
@@ -1,9 +1,11 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import skimage
 import skimage.morphology
 import skimage.feature
+from matplotlib.pyplot import figure
 
 
 def processImage(image):
@@ -37,13 +39,19 @@ originals = []
 for f in files:
     originals.append(mpimg.imread(f))
 
-rows = 2
+rows = 3
 cols = len(originals) / rows
 
-f, axarray = plt.subplots(rows, cols)
+f, axarray = plt.subplots(rows, cols, gridspec_kw={'wspace': 0, 'hspace': 0})
+
+for axis in f.axes:
+    axis.get_xaxis().set_visible(False)
+    axis.get_yaxis().set_visible(False)
+    axis.margins(0)
+
 for r in range(rows):
     for c in range(cols):
         axarray[r, c].imshow(processImage(
             originals[c + r*cols]), cmap="gray")
 
-plt.show()
+plt.savefig("planes.png", dpi=1600, bbox_inches='tight')
